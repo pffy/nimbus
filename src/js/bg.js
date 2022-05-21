ca11d47878179a67a1dd9d04ca186dbf5b5d9d97
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2022-05-21 11:48:40 -0700

    Update bg.js

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,10 +1,16 @@
+/**
+ * name     : bg.js
+ * task     : service worker
+ * git      : https://github.com/pffy/nimbus
+ * author   : The Pffy Authors https://pffy.dev
+ * license  : https://opensource.org/licenses/MIT
+ */
+
 console.log('bg-js-sayinghello');
-
-
 
 chrome.action.onClicked.addListener(function(){
 	console.log('bg-js-clicked');
- 
+
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
